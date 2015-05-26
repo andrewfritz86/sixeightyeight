741e8c284ebf77803619a3ba489151f375ab0b32
Andrew Fritz <andrew.fritz86@gmail.com>
2015-05-26 13:30:17 -0400

    fixed js load issue

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,7 +6,8 @@ var Bill = Backbone.Model.extend({
 
 var BillView = Backbone.View.extend({
   tagName: 'tr',
-  template: '<td> {{owner}} </td> <td> {{name}} </td>',
+  template: $("#bill-template").html(),
+  // template: '<td> {{name}} </td> <td> {{owner}} </td> <td> {{dom_debt}} </td> <td> {{shamy_debt}} </td> <td> {{jamie_debt}} </td> <td> {{andy_debt}} </td>',
   render: function(){
     var html = Mustache.render(this.template, this.model.attributes)
     this.$el.html(html)
